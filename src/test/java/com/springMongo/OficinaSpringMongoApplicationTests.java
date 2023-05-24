aca916396506bb9d9b8dccdd948b06047959e3ff
Jailine <jay.santana964@gmail.com>
2023-05-24 16:07:47 -0300

    Criação do projeto spring mongoDB

---
+++ src/test/java/com/springMongo/OficinaSpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficinaSpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
