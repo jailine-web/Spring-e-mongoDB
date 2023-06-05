28d97d8db6ff318e4c1f87773bb121589176fe4f
Jailine <jay.santana964@gmail.com>
2023-06-05 14:26:19 -0300

    Criação da classe Usuário DTO

--- src/main/java/com/springMongo/resources/UsuarioController.java
+++ src/main/java/com/springMongo/resources/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.springMongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springMongo.DTO.UsuarioDTO;
 import com.springMongo.dominio.Usuario;
 import com.springMongo.service.UsuarioServico;
 
@@ -21,14 +23,17 @@ public class UsuarioController {
 	private UsuarioServico servico;
 	
 @RequestMapping(method=RequestMethod.GET)
-public ResponseEntity<List<Usuario>> encontrarTodosUsuarios(){
+public ResponseEntity<List<UsuarioDTO>> encontrarTodosUsuarios(){
 	
-	Usuario maria = new Usuario("1", "Maria Silva", "maria@gmail.com");
+	//Usuario maria = new Usuario("1", "Maria Silva", "maria@gmail.com");
 	//Usuario alex = new Usuario("2", "Alex Santos", "alex@gmail.com");
 	
-	servico.salvarDados(maria);
+	//servico.salvarDados(maria);
+	
 	List<Usuario> lista = servico.encontrarTodos();
-	return ResponseEntity.ok().body(lista);
+	List<UsuarioDTO> listaDTO = lista.stream().
+			map(x -> new UsuarioDTO(x)).collect(Collectors.toList());
+	return ResponseEntity.ok().body(listaDTO);
 	
 }
 @PostMapping
