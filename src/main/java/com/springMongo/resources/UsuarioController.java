80b8e709edb418ccee7ebf45278ad9050ee5b87f
Jailine <jay.santana964@gmail.com>
2023-06-07 16:57:59 -0300

    Endpoint para retornar posts de um usuário

--- src/main/java/com/springMongo/resources/UsuarioController.java
+++ src/main/java/com/springMongo/resources/UsuarioController.java
@@ -7,7 +7,6 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.springMongo.DTO.UsuarioDTO;
+import com.springMongo.dominio.Post;
 import com.springMongo.dominio.Usuario;
 import com.springMongo.service.UsuarioServico;
 
@@ -70,4 +70,11 @@ public ResponseEntity<Void> atualizacao(@RequestBody UsuarioDTO usuarioDto, @Pat
 	return ResponseEntity.noContent().build();
 }	
 
+@RequestMapping(value="/{id}/posts",method=RequestMethod.GET)
+public ResponseEntity<List<Post>> encontrarPosts(@PathVariable String id){
+	
+	Usuario usuario = servico.buscarPorId(id);
+	return ResponseEntity.ok().body(usuario.getPosts());
+	
+}
 }
